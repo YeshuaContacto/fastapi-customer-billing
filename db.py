0b595604b58f06a4676c933961777a73f2f0f90e
Yeshua Sanchez <yeshuacontacto@gmail.com>
2025-07-24 17:50:09 -0600

    integracion: listar y crear customers con SQLModel

--- db.py
+++ db.py
@@ -1,13 +1,17 @@
+from fastapi import FastAPI
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import Session, create_engine
+from sqlmodel import Session, create_engine, SQLModel
 
-aqlite_name = "db.sqlite3"
-sqlite_url = f"sqlite:///{aqlite_name}"
+sqlite_name = "db.sqlite3"
+sqlite_url = f"sqlite:///{sqlite_name}"
 
 engine = create_engine(sqlite_url)
 
+def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
 
 def get_session():
     with Session(engine) as session:
