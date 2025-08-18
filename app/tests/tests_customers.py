8a1aced5a7c2662960a96b9a17bb9cf15c843eb6
Yeshua Sanchez <yeshuacontacto@gmail.com>
2025-08-17 19:57:44 -0600

    Creación de prueba test_delete_customer

--- app/tests/tests_customers.py
+++ app/tests/tests_customers.py
@@ -72,7 +72,27 @@ def test_update_customer(client):
     assert response_read.json()["age"] == 26
 
 
+def test_delete_customer(client):
+    # 1. Crear un cliente
+    response = client.post(
+        "/customers",
+        json={
+            "name": "Alice",
+            "email": "alice@example.com",
+            "age": 28,
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    customer_id = response.json()["id"]
 
+    # 2. Eliminar el cliente
+    response_delete = client.delete(f"/customers/{customer_id}")
+    assert response_delete.status_code == status.HTTP_200_OK
+    assert response_delete.json()["Detail"] == "ok"
+
+    # 3. Intentar leer el cliente borrado
+    response_read = client.get(f"/customers/{customer_id}")
+    assert response_read.status_code == status.HTTP_404_NOT_FOUND
 
 
 
